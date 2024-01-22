e51f9e0c424d192a3e75b1e77ab4701629c03502
Jeffrey Goldberg <jeff@agilebits.com>
2024-01-21 21:17:47 -0600

    restarting yet again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,14 +9,15 @@
 project = 'Powerset generator'
 copyright = '2024, Jeffrey Goldberg'
 author = 'Jeffrey Goldberg'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
