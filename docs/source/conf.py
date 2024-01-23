e1ac834f96c483da6412f5d833f0779441ef5919
Jeffrey Goldberg <jeff@agilebits.com>
2024-01-23 14:32:44 -0600

    sphinx conf type annotations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,17 +25,17 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
+extensions: list[str] = [
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
 ]
 
-templates_path = ["_templates"]
-exclude_patterns = []
+templates_path: list[str] = ["_templates"]
+exclude_patterns: list[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
-html_static_path = ["_static"]
+html_static_path: list[str] = ["_static"]
