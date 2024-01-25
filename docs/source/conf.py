b88a0317cbc69b9e56c850ed5d79c67c7a8c7e0c
Jeffrey Goldberg <jeff@agilebits.com>
2024-01-24 20:30:14 -0600

    conf is type hinted

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ intersphinx_mapping = {
 }
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns: list[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
